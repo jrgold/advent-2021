5b0b38a1dc6fa6cc167afed4db1a5121291dbc68
jrgold <jrgold@users.noreply.github.com>
2021-12-02 16:19:22 +1100

    two

---
+++ 2.ex
@@ -0,0 +1,28 @@
+defmodule Advent2 do
+  def read_commands(file_name) do
+    file_name
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      case String.split(line, " ") do
+        ["forward", x] -> {String.to_integer(x), 0}
+        ["up",      y] -> {0, -String.to_integer(y)}
+        ["down",    y] -> {0, String.to_integer(y) }
+      end
+    end)
+  end
+
+  def sum_commands(commands) do
+    commands
+    |> Enum.reduce(fn {d_x, d_y}, {x0, y0} -> {x0 + d_x, y0 + d_y} end)
+  end
+
+  def aim_commands(commands) do
+    commands
+    |> Enum.reduce({0, 0, 0}, fn {d_x, d_aim}, {aim0, x0, y0} -> {aim0 + d_aim, x0 + d_x, y0 + aim0 * d_x} end)
+  end
+end
+
+input = Advent2.read_commands("input/2.txt")
+input |> Advent2.sum_commands |> then(fn {x, y} -> x * y end) |> IO.puts
+input |> Advent2.aim_commands |> then(fn {_aim, x, y} -> x * y end) |> IO.puts
